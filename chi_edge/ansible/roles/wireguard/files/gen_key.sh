976962c808f052ba5eecd05330f8d5ada80185e8
Jason Anderson <jasonanderson@uchicago.edu>
2021-11-09 19:39:20 -0600

    Fixup tunnel gen

--- chi_edge/ansible/roles/wireguard/files/gen_key.sh
+++ chi_edge/ansible/roles/wireguard/files/gen_key.sh
@@ -3,3 +3,4 @@
 # Saves a private key to a destination DEST. If PRIV_KEY is provided,
 # this is used as the saved key; otherwise, one is generated via `wg genkey`.
 echo "${2:-$(wg genkey)}" >"$1"
+chmod 400 "$1"
