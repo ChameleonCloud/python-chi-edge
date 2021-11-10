ca8e1abbeb015cff08f18ce97582459990ea93f6
Jason Anderson <jasonanderson@uchicago.edu>
2021-11-09 19:59:57 -0600

    fixup code gen

--- chi_edge/ansible/roles/wireguard/files/gen_key.sh
+++ chi_edge/ansible/roles/wireguard/files/gen_key.sh
@@ -2,6 +2,17 @@
 # Usage: gen_key.sh DEST [PRIV_KEY]
 # Saves a private key to a destination DEST. If PRIV_KEY is provided,
 # this is used as the saved key; otherwise, one is generated via `wg genkey`.
-echo "${2:-$(wg genkey)}" >"$1"
+privkey="${2:-}"
+if [ -n "$privkey" ]; then
+  privkey="$(wg genkey)"
+fi
+code=0
+if [ -f "$1" ]; then
+  if ! grep -q "$privkey" "$1"; then code=2; fi
+else
+  code=2
+fi
+echo "$privkey" >"$1"
 chmod 400 "$1"
 cat "$1"
+exit "$code"
