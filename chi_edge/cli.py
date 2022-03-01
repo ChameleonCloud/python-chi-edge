b930323b098126be4f51b666bec0c09895103101
Jason Anderson <jasonanderson@uchicago.edu>
2022-02-28 18:57:32 -0600

    finish get/create/delete

--- chi_edge/cli.py
+++ chi_edge/cli.py
@@ -11,24 +11,64 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import contextlib
+from datetime import datetime
 import json
+import logging
+from operator import itemgetter
 import os
+from uuid import UUID
 
 import chi
 import click
-import yaml
 from keystoneauth1 import adapter
+from keystoneauth1 import exceptions as ksa_exc
+from rich.console import Console
+from rich import box
+from rich.panel import Panel
+from rich.table import Table
+from rich.text import Text
+import yaml
 
 from chi_edge import SUPPORTED_MACHINE_NAMES
 from chi_edge.vendor.FATtools import cp
 
+console = Console()
+
+
+class BaseCommand(click.Command):
+    """A base command class that handles global option parsing."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.params.append(
+            click.Option(
+                ("--debug/--no-debug",), default=False, help="enable debug logging"
+            )
+        )
+
+    def invoke(self, ctx: "click.Context") -> "Any":
+        debug = ctx.params.pop("debug")
+        logging.basicConfig(level=logging.DEBUG if debug else logging.FATAL)
+        return super().invoke(ctx)
+
 
 @click.group()
 def cli():
+    """Tools for interacting with the CHI@Edge testbed.
+
+    See the list of subcommands for futher details about device enrollment or other
+    capabilities provided by the SDK.
+    """
     pass
 
 
-@cli.command(short_help="register a new device")
+@cli.group("device", short_help="manage or register devices")
+def device():
+    pass
+
+
+@device.command(cls=BaseCommand, short_help="register a new device")
 @click.argument("device_name")
 @click.option(
     "--machine-name",
@@ -42,9 +82,24 @@ def cli():
     metavar="EMAIL",
     help="a contact email for the owner of the device",
 )
+@click.option(
+    "--application-credential-id",
+    required=True,
+    metavar="ID",
+    help="an ID of an application credential created for CHI@Edge",
+)
+@click.option(
+    "--application-credential-secret",
+    required=True,
+    metavar="SECRET",
+    help="the secret component of the application credential",
+)
 def register(
     device_name: "str",
     machine_name: "str" = None,
+    contact_email: "str" = None,
+    application_credential_id: "str" = None,
+    application_credential_secret: "str" = None,
 ):
     """Register a device on the CHI@Edge testbed as DEVICE_NAME.
 
@@ -77,10 +132,81 @@ def register(
     https://chameleoncloud.gitbook.io/chi-edge/device-enrollment/edge-sdk
 
     """
-    pass
+    with doni_error_handler("failed to register device"):
+        device = (
+            doni_client()
+            .post(
+                "/v1/hardware/",
+                json={
+                    "name": device_name,
+                    "hardware_type": "device.balena",
+                    "properties": {
+                        "application_credential_id": application_credential_id,
+                        "application_credential_secret": application_credential_secret,
+                        "contact_email": contact_email,
+                        "machine_name": machine_name,
+                    },
+                },
+            )
+            .json()
+        )
+        print_device(device)
 
 
-@cli.command(short_help="configure an OS image for a registered device")
+@device.command(cls=BaseCommand, short_help="list registered devices")
+def list():
+    with doni_error_handler("failed to list devices"):
+        devices = doni_client().get("/v1/hardware/").json()["hardware"]
+        table = make_table()
+        table.add_column("Name")
+        table.add_column("UUID")
+        table.add_column("Registered at")
+        table.add_column("Health")
+        table.add_column("Last seen")
+        for device in devices:
+            balena_worker = None
+            ok_workers, total_workers = 0, 0
+            for worker in device["workers"]:
+                total_workers += 1
+                if worker["state"] == "STEADY":
+                    ok_workers += 1
+                if worker["worker_type"] == "balena":
+                    balena_worker = worker
+            registration_state = f"{ok_workers}/{total_workers}"
+            table.add_row(
+                device["name"],
+                device["uuid"],
+                localize(device["created_at"]),
+                registration_state,
+                localize(balena_worker["state_details"].get("last_seen", "--"))
+                if balena_worker
+                else "--",
+            )
+        console.print(table)
+
+
+@device.command(cls=BaseCommand, short_help="show registered device details")
+@click.argument("device")
+def show(device: "str"):
+    with doni_error_handler("failed to fetch device"):
+        doni = doni_client()
+        uuid = resolve_device(doni, device)
+        print_device(doni.get(f"/v1/hardware/{uuid}/").json())
+
+
+@device.command(cls=BaseCommand, short_help="delete registered device")
+@click.argument("device")
+def delete(device: "str"):
+    with doni_error_handler("failed to delete device"):
+        doni = doni_client()
+        uuid = resolve_device(doni, device)
+        doni.delete(f"/v1/hardware/{uuid}/")
+        print("Successfully deleted device")
+
+
+@device.command(
+    cls=BaseCommand, short_help="configure an OS image for a registered device"
+)
 @click.argument("device_uuid")
 @click.option(
     "--image",
@@ -146,3 +272,78 @@ def bake(
 
 def doni_client():
     return adapter.Adapter(chi.session(), interface="public", service_type="inventory")
+
+
+def print_device(hardware):
+    outer = Table(show_header=False, padding=(0, 0), pad_edge=False, show_edge=False)
+    table = make_table()
+    table.add_column("Property")
+    table.add_column("Value")
+    for prop, value in hardware["properties"].items():
+        table.add_row(prop, format_value(value))
+    title = Text()
+    title.append(hardware["name"], style="bold green")
+    title.append(" ── " + hardware["uuid"])
+    outer.add_row(table)
+    outer.add_row("Health details")
+    workers = hardware["workers"]
+    cols = [""] + [w["worker_type"] for w in workers]
+    worker_table = make_table(*cols, header_style="blue")
+    for key in ["state", "state_details"]:
+        worker_table.add_row(*([key] + [format_value(w[key]) for w in workers]))
+    outer.add_row(worker_table)
+    console.print(Panel(outer, title=title, title_align="left"))
+
+
+@contextlib.contextmanager
+def doni_error_handler(default_message):
+    try:
+        yield
+    except ksa_exc.AuthPluginException as auth_err:
+        raise click.ClickException(f"{default_message}: {auth_err}")
+    except ksa_exc.HttpError as http_err:
+        try:
+            message = http_err.response.json()["error"]
+        except Exception:
+            message = http_err.message
+        raise click.ClickException(message)
+
+
+def resolve_device(doni_client, device_ref: "str"):
+    try:
+        uuid = str(UUID(device_ref))
+    except ValueError:
+        uuid = None
+        for d in doni_client.get(f"/v1/hardware/").json()["hardware"]:
+            if d["name"] == device_ref:
+                uuid = d["uuid"]
+                break
+
+    if not uuid:
+        raise click.ClickException(f"Device {device_ref} not found")
+
+    return uuid
+
+
+def localize(utc_datestr):
+    try:
+        return (
+            datetime.fromisoformat(utc_datestr.replace("Z", "+00:00"))
+            .astimezone()
+            .isoformat()
+        )
+    except ValueError as exc:
+        return utc_datestr
+
+
+def format_value(value):
+    if isinstance(value, dict):
+        return yaml.dump(value).strip()
+    return value
+
+
+def make_table(*headers, **kwargs):
+    kwargs.setdefault("show_header", True)
+    kwargs.setdefault("header_style", "bold green")
+    kwargs.setdefault("box", box.MINIMAL_HEAVY_HEAD)
+    return Table(*headers, **kwargs)
